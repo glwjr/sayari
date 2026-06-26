5b763a5e19566e7db29d08b4867218591498250a
Gary White <glw494@gmail.com>
2026-06-25 20:03:15 -0700

    chore: create .env.example files

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -5,10 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { SeedModule } from './database/seeds/seed.module';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
